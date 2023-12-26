14779a0764f0728b2cc4dcfc4431545dfc73f3e7
harshahossain <harshahossain@gmail.com>
2023-12-26 22:12:41 +0600

    created navbar & added assets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
