eb8d3849ed31fdace5a4178c77f52787f11652aa
harshahossain <harshahossain@gmail.com>
2023-12-28 21:24:54 +0600

    updated

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -1,5 +1,18 @@
 import "./Offers.css";
+import exclusiveImg from "../Assets/exclusive_image.png";
 
 export default function Offers() {
-  return <div className="offers"></div>;
+  return (
+    <div className="offers">
+      <div className="offers-left">
+        <h1>Exclusive</h1>
+        <h1>Offers For You</h1>
+        <p>ONLY ON BEST SELLERS</p>
+        <button>Check Now</button>
+      </div>
+      <div className="offers-right">
+        <img src={exclusiveImg} alt="exclusive image" />
+      </div>
+    </div>
+  );
 }
