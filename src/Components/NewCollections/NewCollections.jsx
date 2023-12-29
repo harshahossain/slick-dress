6689d47d28562e78868cb0952b0eb3e7c11f6df9
harshahossain <harshahossain@gmail.com>
2023-12-29 20:33:25 +0600

    added NewCollections and NewsLetter

---
+++ src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,26 @@
+import "./NewCollections.css";
+import new_collections from "../Assets/new_collections";
+import Item from "../Item/Item";
+
+export default function NewCollections() {
+  return (
+    <div className="new-collections">
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections">
+        {new_collections.map((item, i) => {
+          return (
+            <Item
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              newPrice={item.new_price}
+              oldPrice={item.old_price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
