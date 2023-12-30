c852a2195f27729dc118e399691c608a24fc8876
harshahossain <harshahossain@gmail.com>
2023-12-30 23:27:40 +0600

    updated

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -1,5 +1,43 @@
 import "./ProductDetails.css";
+import starIcon from "../Assets/star_icon.png";
+import starDullIcon from "../Assets/star_dull_icon.png";
 
-export default function ProductDetails() {
-  return <div className="productdisplay"></div>;
+export default function ProductDetails({ product }) {
+  return (
+    <div className="productdisplay">
+      <div className="productdisplay-left"></div>
+      <div className="productdisplay-img-list">
+        <img src={product.image} alt="product image" />
+        <img src={product.image} alt="product image" />
+        <img src={product.image} alt="product image" />
+        <img src={product.image} alt="product image" />
+      </div>
+      <div className="productdisplay-img">
+        <img
+          className="productdisplay-main-img"
+          src={product.image}
+          alt="product image"
+        />
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-star">
+          <img src={starIcon} alt="Star Icon" />
+          <img src={starIcon} alt="Star Icon" />
+          <img src={starIcon} alt="Star Icon" />
+          <img src={starIcon} alt="Star Icon" />
+          <img src={starDullIcon} alt="Star Icon dull" />
+          <p>(122)</p>
+        </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-oldprice">
+            ${product.old_price}
+          </div>
+          <div className="productdisplay-right-newprice">
+            ${product.new_price}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { ShopContext } from "../Context/ShopContext";
 import { useParams } from "react-router-dom";
 import Breadcrum from "../Components/Breadcrums/Breadcrum";
+import ProductDetails from "../Components/ProductDetails/ProductDetails";
 
 export default function Product() {
   const { all_product } = useContext(ShopContext);
@@ -10,6 +11,7 @@ export default function Product() {
   return (
     <div>
       <Breadcrum product={product} />
+      <ProductDetails product={product} />
     </div>
   );
 }
