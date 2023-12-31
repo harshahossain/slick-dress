fb882263b1031bb14bbca4ade8ec525245353213
harshahossain <harshahossain@gmail.com>
2023-12-31 13:40:34 +0600

    updated

--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -1,13 +1,20 @@
 import "./Breadcrum.css";
-import arrowIcon from "../Assets/arrow.png";
+import arrowIcon from "../Assets/breadcrum_arrow.png";
+import { Link } from "react-router-dom";
 export default function Breadcrum({ product }) {
   return (
     <div className="breadcrum">
-      HOME <img src={arrowIcon} alt="arrow pointing right" />
-      SHOP
-      <img src={arrowIcon} alt="arrow pointing right" />
-      {product.category}
-      <img src={arrowIcon} alt="arrow pointing right" />
+      <Link to="/">
+        <span>HOME</span> <img src={arrowIcon} alt="arrow pointing right" />
+      </Link>
+      <Link to="/">
+        <span>SHOP</span>
+        <img src={arrowIcon} alt="arrow pointing right" />{" "}
+      </Link>
+      <Link to={`/${product.category}s`}>
+        <span>{product.category}</span>
+        <img src={arrowIcon} alt="arrow pointing right" />
+      </Link>
       {product.name}
     </div>
   );
